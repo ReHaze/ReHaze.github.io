aa77eed16af8e7f787721104052b4dde6a1dd8b8
ReHaze <apexraizy@gmail.com>
2019-01-08 09:24:11 +0700

    edited layout and fix some bugs

--- Random_Quote_Machine/scripts.js
+++ Random_Quote_Machine/scripts.js
@@ -1,30 +1,48 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-	var painText;
-	var colors = ['#16a085',
+	let painText;
+	let colors = ['#16a085',
     '#27ae60', "#73A857",
     '#2c3e50', '#f39c12', '#e74c3c', 
     '#9b59b6', '#FB6964', '#342224',
     "#472E32", "#BDBB99", "#77B1A9"
 	];
-	var button = document.querySelector('.btn');
-	var author = document.querySelector('.author');
-	var wrapper = document.querySelector('.wrapper');
-	var textForQuote = document.querySelector('.text');
-	var req = new XMLHttpRequest();
+	let button = document.querySelector('.btn');
+	let author = document.querySelector('.author');
+	let wrapper = document.querySelector('.wrapper');
+	let textForQuote = document.querySelector('.text');
+	let req = new XMLHttpRequest();
 	req.open('GET', 'quotes.json');
 	req.onload = function() {
-    	var quote = "";
+    	let quote = "";
     	painText = JSON.parse(req.responseText);
     	generate();
 	}
 	req.send();
 
 	function generate(){
-		var quote = painText.quotes[Math.floor(Math.random() * painText.quotes.length)];
+		let quote = painText.quotes[Math.floor(Math.random() * painText.quotes.length)];
+		textForQuote.classList.remove("animate");
+		void textForQuote.offsetWidth;
+		textForQuote.classList.add("animate");
 		textForQuote.innerHTML = quote.quote;
 		author.innerHTML = quote.author;
-		wrapper.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+		author.classList.remove("animate");
+		void author.offsetWidth;
+		author.classList.add("animate");
+		let color = wrapper.style.backgroundColor;
+		wrapper.style.backgroundColor = generateColor(color);
 		button.style.backgroundColor = wrapper.style.backgroundColor;
+	}
+
+	function generateColor(e){
+		let currentColor = e;
+		let newColor = colors[Math.floor(Math.random() * colors.length)];	
+		if (currentColor != newColor) {
+			return newColor;
+		}
+		else {
+			generateColor(e);
+		}
 	}
 	
 	button.addEventListener("click", generate);
