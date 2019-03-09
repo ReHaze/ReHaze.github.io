b394f9eeba9c91f648bd353f9cafdcb1dff81681
ReHaze <apexraizy@gmail.com>
2019-03-09 17:14:57 +0700

    fixed

--- render.js
+++ render.js
@@ -1,33 +1,15 @@
-'use strict'
-	var menu = document.querySelector(".main-menu__list");
+"use strict";
+let menu = document.querySelector(".main-menu__list");
+let button = document.querySelector(".main-menu__close");
 
-	class Toggle extends React.Component {
-  		constructor(props) {
-			super(props);
-			this.handleClick = this.handleClick.bind(this);
-			this.state = {
-				classes: 'main-menu__close'
-			}
-  		}
-    handleClick() {
-    	if (menu.classList.contains("hidden-item")) {
-				menu.classList.remove("hidden-item");
-				this.setState({
-					classes: 'main-menu__close'
-				})
-				
-		}	
-		else {
-			menu.classList.add("hidden-item");
-			this.setState({
-					classes: 'main-menu__open'
-				})
-		}
-  	}
-  	render() {
-    	return (
-			  <button className={this.state.classes} onClick={this.handleClick}></button>);
-  		}
-	}	
-
-	ReactDOM.render(<Toggle/>, document.getElementById('root') );
+button.onclick = e => {
+    if (menu.classList.contains("hidden-item")) {
+        menu.classList.remove("hidden-item");
+        button.classList.remove("main-menu__open");
+        button.classList.add("main-menu__close");
+    } else {
+        menu.classList.add("hidden-item");
+        button.classList.add("main-menu__open");
+        button.classList.remove("main-menu__close");
+    }
+};
