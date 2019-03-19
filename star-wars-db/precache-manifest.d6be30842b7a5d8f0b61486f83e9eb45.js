0c88d88468b32370a61ff28f5fde9b86946bdff8
ReHaze <apexraizy@gmail.com>
2019-03-20 03:33:01 +0700

    added react app

---
+++ star-wars-db/precache-manifest.d6be30842b7a5d8f0b61486f83e9eb45.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "d8ab6dea99cc4600b2d55d84a0609de2",
+    "url": "/ReHaze/star-wars-db/static/media/Starjhol.d8ab6dea.woff"
+  },
+  {
+    "revision": "85f6c66823c500dc6d12aad531308e10",
+    "url": "/ReHaze/star-wars-db/static/media/Starjhol.85f6c668.eot"
+  },
+  {
+    "revision": "72efad9da193a0e6549e1dcd32984011",
+    "url": "/ReHaze/star-wars-db/static/media/Starjhol.72efad9d.svg"
+  },
+  {
+    "revision": "53c0bf82bd8ad72a1c6cf78c6517292a",
+    "url": "/ReHaze/star-wars-db/static/media/Starjhol.53c0bf82.ttf"
+  },
+  {
+    "revision": "5a7c4c55bef22415634e",
+    "url": "/ReHaze/star-wars-db/static/js/runtime~main.5a7c4c55.js"
+  },
+  {
+    "revision": "dc303f36376959aaaeda",
+    "url": "/ReHaze/star-wars-db/static/js/main.dc303f36.chunk.js"
+  },
+  {
+    "revision": "e5c12e2a06d25c015435",
+    "url": "/ReHaze/star-wars-db/static/js/1.e5c12e2a.chunk.js"
+  },
+  {
+    "revision": "dc303f36376959aaaeda",
+    "url": "/ReHaze/star-wars-db/static/css/main.cc60dca0.chunk.css"
+  },
+  {
+    "revision": "2bd9282a37bde5a6dd882b0505ff75ff",
+    "url": "/ReHaze/star-wars-db/index.html"
+  }
+];
