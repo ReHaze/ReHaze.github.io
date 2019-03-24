343f418190baabf6fbad5fad0041a24c1c604e45
ReHaze <apexraizy@gmail.com>
2019-03-24 12:19:28 +0700

    testing

---
+++ star-wars-db/precache-manifest.d413faf1ad5e16812f53b102c8969b5c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "d8ab6dea99cc4600b2d55d84a0609de2",
+    "url": "/static/media/Starjhol.d8ab6dea.woff"
+  },
+  {
+    "revision": "00be5122b6c0e7e1bc65",
+    "url": "/static/css/main.b0ec9304.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d2fba2b27fb4c7c38244",
+    "url": "/static/js/2.e435b9ca.chunk.js"
+  },
+  {
+    "revision": "85f6c66823c500dc6d12aad531308e10",
+    "url": "/static/media/Starjhol.85f6c668.eot"
+  },
+  {
+    "revision": "00be5122b6c0e7e1bc65",
+    "url": "/static/js/main.5e081c1d.chunk.js"
+  },
+  {
+    "revision": "53c0bf82bd8ad72a1c6cf78c6517292a",
+    "url": "/static/media/Starjhol.53c0bf82.ttf"
+  },
+  {
+    "revision": "72efad9da193a0e6549e1dcd32984011",
+    "url": "/static/media/Starjhol.72efad9d.svg"
+  },
+  {
+    "revision": "5ae9e0cbe24b9b3f0fe11d5ad0306521",
+    "url": "/static/media/AvenirLTStd-Medium.5ae9e0cb.woff"
+  },
+  {
+    "revision": "43af39b1229652f4e442f6a79757764e",
+    "url": "/static/media/stars.43af39b1.png"
+  },
+  {
+    "revision": "edd414d969e7bb657833556db6f87d5c",
+    "url": "/index.html"
+  }
+];
