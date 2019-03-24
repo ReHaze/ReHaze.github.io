5b18ac7627c0897d5b32cb7f88a7c05f83685fd4
ReHaze <apexraizy@gmail.com>
2019-03-24 12:30:20 +0700

    fake

---
+++ star-wars-db/precache-manifest.bbd340fc54073aa068e4eaf4a3431c12.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "d8ab6dea99cc4600b2d55d84a0609de2",
+    "url": "/star-wars-db/static/media/Starjhol.d8ab6dea.woff"
+  },
+  {
+    "revision": "7d8aaea3932903a5ed1e",
+    "url": "/star-wars-db/static/css/main.8e6c8153.chunk.css"
+  },
+  {
+    "revision": "69a518fa9cf0c766a323",
+    "url": "/star-wars-db/static/js/runtime~main.5a8b5f29.js"
+  },
+  {
+    "revision": "dd9b3f92862b00716623",
+    "url": "/star-wars-db/static/js/2.cc33979e.chunk.js"
+  },
+  {
+    "revision": "85f6c66823c500dc6d12aad531308e10",
+    "url": "/star-wars-db/static/media/Starjhol.85f6c668.eot"
+  },
+  {
+    "revision": "7d8aaea3932903a5ed1e",
+    "url": "/star-wars-db/static/js/main.10ae4bef.chunk.js"
+  },
+  {
+    "revision": "72efad9da193a0e6549e1dcd32984011",
+    "url": "/star-wars-db/static/media/Starjhol.72efad9d.svg"
+  },
+  {
+    "revision": "5ae9e0cbe24b9b3f0fe11d5ad0306521",
+    "url": "/star-wars-db/static/media/AvenirLTStd-Medium.5ae9e0cb.woff"
+  },
+  {
+    "revision": "53c0bf82bd8ad72a1c6cf78c6517292a",
+    "url": "/star-wars-db/static/media/Starjhol.53c0bf82.ttf"
+  },
+  {
+    "revision": "43af39b1229652f4e442f6a79757764e",
+    "url": "/star-wars-db/static/media/stars.43af39b1.png"
+  },
+  {
+    "revision": "70b7ee6ff6bbe70802a39330d57e0a48",
+    "url": "/star-wars-db/index.html"
+  }
+];
--- star-wars-db/service-worker.js
+++ star-wars-db/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d413faf1ad5e16812f53b102c8969b5c.js"
+  "/star-wars-db/precache-manifest.bbd340fc54073aa068e4eaf4a3431c12.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/star-wars-db/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
