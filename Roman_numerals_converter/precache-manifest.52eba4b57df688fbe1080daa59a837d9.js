587142fab6d0fce75289442ba8c6574ddb6ca9ac
ReHaze <apexraizy@gmail.com>
2019-03-26 18:47:46 +0700

    added Material UI

---
+++ Roman_numerals_converter/precache-manifest.52eba4b57df688fbe1080daa59a837d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7221c06be9bae94a5014",
+    "url": "/Roman_numerals_converter/static/js/runtime~main.7221c06b.js"
+  },
+  {
+    "revision": "8d566e2a457a674ceeb4",
+    "url": "/Roman_numerals_converter/static/js/main.8d566e2a.chunk.js"
+  },
+  {
+    "revision": "2431ce83d16c5c395949",
+    "url": "/Roman_numerals_converter/static/js/1.2431ce83.chunk.js"
+  },
+  {
+    "revision": "8d566e2a457a674ceeb4",
+    "url": "/Roman_numerals_converter/static/css/main.fe1759ad.chunk.css"
+  },
+  {
+    "revision": "edd26e560ee2c28283558ff089e1c764",
+    "url": "/Roman_numerals_converter/index.html"
+  }
+];
