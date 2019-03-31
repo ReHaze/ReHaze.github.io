e96aee7be47f55ebda0147d4eef27d2c329279c3
ReHaze <apexraizy@gmail.com>
2019-03-31 16:07:15 +0700

    fixed IE

--- render.js
+++ render.js
@@ -2,14 +2,14 @@
 let menu = document.querySelector(".main-menu__list");
 let button = document.querySelector(".main-menu__close");
 
-button.onclick = e => {
-    if (menu.classList.contains("hidden-item")) {
-        menu.classList.remove("hidden-item");
-        button.classList.remove("main-menu__open");
-        button.classList.add("main-menu__close");
-    } else {
-        menu.classList.add("hidden-item");
-        button.classList.add("main-menu__open");
-        button.classList.remove("main-menu__close");
-    }
+button.onclick = function() {
+  if (menu.classList.contains("hidden-item")) {
+    menu.classList.remove("hidden-item");
+    button.classList.remove("main-menu__open");
+    button.classList.add("main-menu__close");
+  } else {
+    menu.classList.add("hidden-item");
+    button.classList.add("main-menu__open");
+    button.classList.remove("main-menu__close");
+  }
 };
