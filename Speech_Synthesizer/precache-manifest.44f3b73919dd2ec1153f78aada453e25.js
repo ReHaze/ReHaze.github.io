b736d15d45d6bc8424592f9dcc9eaf42bda29ca7
ReHaze <apexraizy@gmail.com>
2019-04-13 05:08:40 +0700

    added new project

---
+++ Speech_Synthesizer/precache-manifest.44f3b73919dd2ec1153f78aada453e25.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b9c0abdcf9d46e337620f9e0f78f7870",
+    "url": "/Speech_Synthesizer/static/media/stop.b9c0abdc.svg"
+  },
+  {
+    "revision": "66563b709e0d74daf2506e4893d385f1",
+    "url": "/Speech_Synthesizer/static/media/play.66563b70.svg"
+  },
+  {
+    "revision": "b52a4c237643f5a72890",
+    "url": "/Speech_Synthesizer/static/js/runtime~main.d929501b.js"
+  },
+  {
+    "revision": "774546c1128626fa1091",
+    "url": "/Speech_Synthesizer/static/js/main.b95ce9ce.chunk.js"
+  },
+  {
+    "revision": "71c055b098b64c11c0a2",
+    "url": "/Speech_Synthesizer/static/js/2.9ed7e963.chunk.js"
+  },
+  {
+    "revision": "774546c1128626fa1091",
+    "url": "/Speech_Synthesizer/static/css/main.937034a5.chunk.css"
+  },
+  {
+    "revision": "71c055b098b64c11c0a2",
+    "url": "/Speech_Synthesizer/static/css/2.3f13305d.chunk.css"
+  },
+  {
+    "revision": "12a0d6b008c049139e7e8ab14263174a",
+    "url": "/Speech_Synthesizer/index.html"
+  }
+];
